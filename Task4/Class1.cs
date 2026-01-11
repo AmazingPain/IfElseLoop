25c77e5e1c92d17b4cc5d41e37f86cb8d4d522eb
Purity <45917191+AmazingPain@users.noreply.github.com>
2026-01-11 16:22:47 +0300

    Add Task4

---
+++ Task4/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal class Class1
+    {
+        public void Diapozon()
+        {
+            while (true)
+            {
+                Clear();
+                int limit1;
+                int limit2;
+                int countEven = 0;
+                int countUneven = 0;
+                int countSum = 0;
+                int sumLimit = 0;
+                try
+                {
+                    Write("Введите первое число для обозначения диапозона: ");
+                    limit1 = int.Parse(ReadLine());
+                    Write("Введите второе число для обозначения диапозона: ");
+                    limit2 = int.Parse(ReadLine());
+                }
+                catch (Exception)
+                {
+                    WriteLine("Неудалось считать данные с ввода (неправильный формат ввода)");
+                    ReadLine();
+                    continue;
+                }
+                WriteLine($"Диапозон равен {limit1} - {limit2}");
+                while (limit1 <= limit2)
+                {
+                    if (limit1 % 2 == 0)
+                    {
+                        ++countEven;
+
+                    }
+                    else
+                    {
+                        ++countUneven;
+                    }
+                    limit1++;
+                    countSum = countEven + countUneven;
+                    sumLimit += limit1;
+                }
+
+                WriteLine($"Кол-во четных чисел = {countEven}");
+                WriteLine($"Кол-во нечетных чисел = {countUneven}");
+                WriteLine($"Сумма четных и нечетных чисел = {countSum}");
+                WriteLine($"Сумма значений четных и нечетных чисел = {sumLimit}");
+                ReadLine();
+            }
+        }
+    }
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int number = random.Next(1, 101);
+
+            int sumNumbers  = 0;
+            for (int i = 1; i <= number; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sumNumbers += i;
+                }
+            }
+
+            WriteLine($"Number is : {number}");
+            WriteLine($"The sum of numbers  of 3 and 5 is: {sumNumbers}");
+        }
+    }
+}
