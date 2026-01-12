cfd4179563fc09f7d55e4d8f23633f3c0f5faa9e
Purity <45917191+AmazingPain@users.noreply.github.com>
2026-01-12 18:53:03 +0300

    Add Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Task8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string userPassword = "123";
+            string secretMessage = "Ставка цб 16%";
+            string userInput = "";
+
+            int tryCounter = 3;
+
+            WriteLine("Введите пароль для доступа к секретному сообщению");
+            while (tryCounter > 0)
+            {
+
+                userInput = ReadLine();
+                if (userInput == userPassword)
+                {
+                    WriteLine(secretMessage);
+                    break;
+                }
+                else
+                {
+                    tryCounter--;
+                    WriteLine($"Неверный пароль, попробуйте снова. У вас осталось {tryCounter} попыток(ки)");
+                }
+            }
+            if (userInput != userPassword)
+            {
+                WriteLine("Доступ заблокирован. Попытки исчерпаны.");
+            }
+        }
+    }
+}
