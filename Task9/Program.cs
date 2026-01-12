a9aa186b45759d520f2f64dd8cf7ee25327fbb9a
Purity <45917191+AmazingPain@users.noreply.github.com>
2026-01-12 20:40:51 +0300

    Add Task9

---
+++ Task9/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int N = random.Next(10, 26);
+            int currentValue = 0;
+            int counter = 0;
+
+            while (currentValue < 50)
+            {
+                currentValue += N;
+            }
+
+            for (; 50 <= currentValue && currentValue <= 150; currentValue+=N)
+            {
+                counter++;
+            }
+            WriteLine(N);
+            WriteLine(counter);
+        }
+    }
+}
