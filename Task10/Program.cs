57f67cc218a704d40e57357684ce2a39edd73e06
Purity <45917191+AmazingPain@users.noreply.github.com>
2026-01-12 21:05:25 +0300

    Add Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int genericNumber = random.Next(10, 33);
+            int degreeTwo = 2;
+            int resultDegree = 1;
+            int countDegree = 0;
+
+
+            while (resultDegree <= genericNumber)
+            {
+                resultDegree *= degreeTwo;
+                countDegree++;
+            }
+            WriteLine($"Заданное число {genericNumber}, минимальая степень 2 превосходящее число {countDegree} , число превосходящее заданное число {resultDegree}");
+        }
+    }
+}
